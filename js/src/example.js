be9da5eb147c565db911ee1d874a8b8732f2220a
Martin Sandve Alnæs <martinal@simula.no>
2017-05-03 08:57:21 +0200

    Use ES6 class for view.

--- js/src/example.js
+++ js/src/example.js
@@ -76,7 +76,9 @@ class Unray
 
 // When serialiazing the entire widget state for embedding, only values that
 // differ from the defaults will be specified.
+//class UnrayModel extends widgets.DOMWidgetModel {
 var UnrayModel = widgets.DOMWidgetModel.extend({
+
     defaults: _.extend(_.result(this, 'widgets.DOMWidgetModel.prototype.defaults'), {
         _model_name : 'UnrayModel',
         _view_name : 'UnrayView',
@@ -96,33 +98,38 @@ var UnrayModel = widgets.DOMWidgetModel.extend({
         values: ndarray(new Float32Array(), [0]),
 
         // TODO: More to come
-    })
-}, {
+    }) // end defaults
+
+    //}, {
+    ,
+
     serializers: _.extend({
         coordinates: array_serialization,
         cells: array_serialization,
         values: array_serialization,
     }, widgets.DOMWidgetModel.serializers)
+
 });
+//};
 
 
 // Custom View. Renders the widget model.
-var UnrayView = widgets.DOMWidgetView.extend({
+class UnrayView extends widgets.DOMWidgetView {
 
     /* Hooks called from widget library or backbone */
 
     // Initialize view properties (called on initialization)
-    initialize: function() {
+    initialize() {
         this.log("initialize");
         widgets.DOMWidgetView.prototype.initialize.apply(this, arguments);
 
         this.canvas = null;
         this.gl = null;
         this.unray = null;
-    },
+    }
 
     // Render to DOM (called at least once when placed on page, not sure what the semantics are beyond that?)
-    render: function() {
+    render() {
         this.log("render");
         this.setup_unray(this.el);
         //this.setup_unray_data();
@@ -130,24 +137,24 @@ var UnrayView = widgets.DOMWidgetView.extend({
 
         // Schedule a
         this.schedule_redraw();
-    },
+    }
 
     /* Internal view logic (may contain stupid parts, I don't know the widgets design very well) */
 
-    log: function(msg) {
+    log(msg) {
         console.log("unray view:  " + msg);
-    },
+    }
 
-    wire_events: function() {
+    wire_events() {
         this.log("wire_events");
         this.model.on('change:config', this.config_changed, this);
         this.model.on('change:coordinates', this.coordinates_changed, this);
         this.model.on('change:cells', this.cells_changed, this);
         this.model.on('change:values', this.values_changed, this);
         this.on('animate:update', this.redraw, this);
-    },
+    }
 
-    setup_unray: function(elm) {
+    setup_unray(elm) {
         this.log("setup_unray.");
         if (!this.canvas) {
             var canvas = document.createElement("canvas");
@@ -172,48 +179,48 @@ var UnrayView = widgets.DOMWidgetView.extend({
             this.log("created Unray instance");
         }
         this.log("leaving setup_unray.");
-    },
+    }
 
     // TODO: pythreejs has some more sophisticated animation handlers
-    schedule_redraw: function() {
+    schedule_redraw() {
         window.requestAnimationFrame(_.bind(this.redraw, this));
-    },
+    }
 
     // Update canvas contents by executing gl draw calls in unray
-    redraw: function() {
+    redraw() {
         this.log("redraw()");
         this.unray.redraw();
-    },
+    }
 
     /* Data change handlers */
 
-    config_changed: function() {
+    config_changed() {
         var config = this.model.get('config');
         this.log("config changed:");
         this.log(config);
         this.unray.update_config(config);
         this.schedule_redraw();
-    },
+    }
 
-    coordinates_changed: function() {
+    coordinates_changed() {
         // FIXME
         var coordinates = this.model.get('coordinates');
         this.log("coordinates changed:");
         this.log(coordinates);
         this.unray.update_coordinates(coordinates);
         this.schedule_redraw();
-    },
+    }
 
-    cells_changed: function() {
+    cells_changed() {
         // FIXME
         var cells = this.model.get('cells');
         this.log("cells changed:");
         this.log(cells);
         this.unray.update_cells(cells);
         this.schedule_redraw();
-    },
+    }
 
-    values_changed: function() {
+    values_changed() {
         // FIXME
         var values = this.model.get('values');
         this.log("values changed:");
@@ -222,7 +229,7 @@ var UnrayView = widgets.DOMWidgetView.extend({
         this.schedule_redraw();
     }
 
-});
+};
 
 
 module.exports = {
