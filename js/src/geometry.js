da33fbc660f17236143df245211fbc4627786f40
Martin Sandve Alnæs <martinal@simula.no>
2017-08-17 10:19:19 +0200

    Add more utility geometry constructors, bounding box etc, useful at least for debugging.

--- js/src/geometry.js
+++ js/src/geometry.js
@@ -80,6 +80,7 @@ function create_cells_attribute(cells) {
     );
 }
 
+
 function create_bounding_sphere(coordinates) {
     const bsphere = compute_bounding_sphere(coordinates);
     return new THREE.Sphere(
@@ -99,7 +100,7 @@ function create_bounding_box(coordinates) {
     );
 }
 
-function create_bounding_sphere_geometry(bsphere, scale=1.0, color=0x333333) {
+function create_bounding_sphere_geometry(bsphere, scale=1.0, color=0xcccccc) {
     const geometry = new THREE.SphereGeometry(bsphere.radius * scale, 32, 32);
     const material = new THREE.MeshBasicMaterial({color});
     const mesh = new THREE.Mesh(geometry, material);
@@ -107,64 +108,85 @@ function create_bounding_sphere_geometry(bsphere, scale=1.0, color=0x333333) {
     return mesh;
 }
 
-function create_bounding_box_geometry(bbox, scale=1.0, color=0x333333) {
+function create_bounding_box_geometry(bbox, scale=1.0, color=0xcccccc) {
     const dims = bbox.getSize().toArray().map(x => scale*x);
+
+    // Showing only backside faces avoids hiding the model
+    const side = THREE.BackSide;
+    const material = new THREE.MeshBasicMaterial({color, side});
+
     const geometry = new THREE.BoxGeometry(...dims);
-    const material = new THREE.MeshBasicMaterial({color});
     const mesh = new THREE.Mesh(geometry, material);
     mesh.position.copy(bbox.getCenter());
+
     return mesh;
 }
 
-function create_bounding_box_axis_geometry(bbox, scale=1.0, color=0x000000) {
+function create_bounding_box_midplanes_geometry(bbox, scale=1.0, color=0xcccccc) {
+    const dims = bbox.getSize().toArray().map(x => scale*x);
+
+    const range = [0, 1, 2];
+    const side = THREE.DoubleSide;
+    const colors = range.map(i => color & (0xff << (8*(2-i))));
+    const materials = colors.map(color =>
+        new THREE.MeshBasicMaterial({color, side})
+    );
+    const planes = range.map(i =>
+        new THREE.PlaneGeometry(dims[(i + 1) % 3], dims[(i + 2) % 3])
+    );
+    const meshes = range.map(i =>
+        new THREE.Mesh(planes[i], materials[i])
+    );
+    const angle = Math.PI / 2;
+    meshes[0].rotateY(angle);
+    meshes[0].rotateZ(angle);
+    meshes[1].rotateX(angle);
+    meshes[1].rotateZ(angle);
+    const group = new THREE.Group();
+    meshes.forEach(mesh => group.add(mesh));
+    group.position.copy(bbox.getCenter());
+    return group;
+}
+
+function bounding_box_corners(bbox, scale) {
     const offset = bbox.getSize().toArray().map(x => 0.5*(scale-1.0)*x);
-    const x = bbox.min.toArray();
-    const y = bbox.max.toArray();
+    const u = bbox.min.toArray();
+    const v = bbox.max.toArray();
     for (let i = 0; i < 3; ++i) {
-        x[i] -= offset[i];
-        y[i] += offset[i];
+        u[i] -= offset[i];
+        v[i] += offset[i];
     }
-    
-    const vertices = new Float32Array([
-        // x[0] constant
-        x[0], x[1], x[2],
-        x[0], x[1], y[2],
+    return [u, v];
+}
 
-        x[0], x[1], y[2],
-        x[0], y[1], y[2],
+function box_edge_vertices(u, v) {
+    // The four vertices of the y-z plane if x is fixed
+    const yz = [
+        [u[1], u[2]],
+        [u[1], v[2]],
+        [v[1], v[2]],
+        [v[1], u[2]],
+    ];
+    const points = [];
+    // Hold x fixed and push edges around yz side
+    for (let x of [u[0], v[0]]) {
+        for (let i = 0; i < 4; ++i) {
+            const j = (i + 1) & 3;
+            points.push(x, ...yz[i],
+                        x, ...yz[j]);
+        }
+    }
+    // Push edges from x=min to x=max for each yz vertex
+    for (let i = 0; i < 4; ++i) {
+        points.push(u[0], ...yz[i],
+                    v[0], ...yz[i]);
+    }
+    return new Float32Array(points);
+}
 
-        x[0], y[1], y[2],
-        x[0], y[1], x[2],
-
-        x[0], y[1], x[2],
-        x[0], x[1], x[2],
-
-        // y[0] constant
-        y[0], x[1], x[2],
-        y[0], x[1], y[2],
-
-        y[0], x[1], y[2],
-        y[0], y[1], y[2],
-
-        y[0], y[1], y[2],
-        y[0], y[1], x[2],
-
-        y[0], y[1], x[2],
-        y[0], x[1], x[2],
-
-        // from x[0] to y[0] plane
-        x[0], x[1], x[2],
-        y[0], x[1], x[2],
-
-        x[0], x[1], y[2],
-        y[0], x[1], y[2],
-
-        x[0], y[1], x[2],
-        y[0], y[1], x[2],
-
-        x[0], y[1], y[2],
-        y[0], y[1], y[2],
-    ]);
+function create_bounding_box_axis_geometry(bbox, scale=1.0, color=0x000000) {
+    const [u, v] = bounding_box_corners(bbox, scale);
+    const vertices = box_edge_vertices(u, v);
     const geometry = new THREE.BufferGeometry();
     geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
     const material = new THREE.LineBasicMaterial({
@@ -173,8 +195,7 @@ function create_bounding_box_axis_geometry(bbox, scale=1.0, color=0x000000) {
         depthTest: true,
         depthWrite: true,
         });
-    const mesh = new THREE.LineSegments(geometry, material);
-    return mesh;
+    return new THREE.LineSegments(geometry, material);
 }
 
 export {
@@ -185,5 +206,6 @@ export {
     create_bounding_box,
     create_bounding_sphere_geometry,
     create_bounding_box_geometry,
-    create_bounding_box_axis_geometry
+    create_bounding_box_axis_geometry,
+    create_bounding_box_midplanes_geometry
 };
