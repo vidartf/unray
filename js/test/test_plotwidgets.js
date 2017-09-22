6bace3e51ac31b70af0682bb12b6454e514c81fe
Martin Sandve Alnæs <martinal@simula.no>
2017-09-22 09:38:49 +0200

    Add initial factory function for creation of mesh model, to be extended to all models.

--- js/test/test_plotwidgets.js
+++ js/test/test_plotwidgets.js
@@ -2,22 +2,22 @@
 
 import {assert, expect, should} from 'chai';
 
+import * as ndarray from 'ndarray';
+
 import { MeshModel } from "../src/datawidgets";
 import { SurfacePlotModel } from "../src/plotwidgets";
 
 import { createTestModel } from './testutils';
 
-import * as ndarray from 'ndarray';
+import * as factory from "./modelfactory";
+
 
 describe('plotwidgets', function() {
 
     describe('SurfacePlotModel', function() {
 
         it('should be constructable', function() {
-            const mesh = createTestModel(MeshModel, {
-                cells: ndarray([0, 1, 2, 3]),
-                points: ndarray([0, 0, 0]),
-            });
+            const mesh = factory.mesh();
             const plot = createTestModel(SurfacePlotModel, {mesh});
             expect(plot.get('_model_name')).eq("SurfacePlotModel");
         });
