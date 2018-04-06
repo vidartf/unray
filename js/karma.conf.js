5ebb82a8faefc313ad796773d771f194ba2b7f7f
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-04-06 16:13:17 +0200

    Extend timeouts for karma
    CI can sometimes be slow

--- js/karma.conf.js
+++ js/karma.conf.js
@@ -1,9 +1,13 @@
-
 module.exports = function (config) {
   config.set({
     basePath: '.',
     frameworks: ['mocha', 'karma-typescript'],
     reporters: ['mocha', 'karma-typescript'],
+    client: {
+      mocha: {
+        timeout : 10000, // 10 seconds - upped from 2 seconds
+      }
+    },
     files: [
       { pattern: "test/**/*.ts" },
       { pattern: "src/**/*.ts" }
@@ -11,6 +15,7 @@ module.exports = function (config) {
     preprocessors: {
       '**/*.ts': ['karma-typescript']
     },
+    browserNoActivityTimeout: 31000, // 31 seconds - upped from 10 seconds
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
