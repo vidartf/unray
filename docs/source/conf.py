e425dac72fd6f6f115fc54126dfcc548e8a6d24a
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-04-10 13:12:18 +0200

    Ensure extension included in doc build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -199,10 +199,28 @@ nbsphinx_allow_errors = True # exception ipstruct.py ipython_genutils
 
 
 def setup(app):
+    doc_ext_path = os.path.join(here, '_static', 'unray.js')
+    if not os.path.exists(doc_ext_path):
+        src_file = os.path.join(here, '..', '..', 'js', 'dist', 'index.js')
+        if not os.path.exists(src_file):
+            # Need to build extension
+            from subprocess import check_call
+            popd = os.path.abspath(os.curdir)
+            os.chdir(os.path.join(here, '..', '..', 'js'))
+            try:
+                check_call(['npm', 'install'])
+                check_call(['npm', 'run', 'build'])
+            finally:
+                os.chdir(popd)
+        from shutil import copyfile
+        copyfile(src_file, doc_ext_path)
+
     app.setup_extension('jupyter_sphinx.embed_widgets')
+
     def add_scripts(app):
-        for fname in ['helper.js', 'embed-bundle.js']:
+        for fname in ['unray.js']:
             if not os.path.exists(os.path.join(here, '_static', fname)):
                 app.warn('missing javascript file: %s' % fname)
             app.add_javascript(fname)
+
     app.connect('builder-inited', add_scripts)
